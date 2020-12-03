c61621bfb282bf19ad6de5e7c7e2b5120f106133
Dmitry Mikhalkov <terilloff@gmail.com>
2020-12-03 23:17:47 +0300

    Moving

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import Game from './src/Game.js';
 
 const game = new Game();
-console.log(game);
+window.game = game;
--- src/Game.js
+++ src/Game.js
@@ -28,9 +28,64 @@ export default class Game {
     x: 0,
     y: 0,
     blocks: [
-      [0, 0, 0],
-      [0, 0, 0],
+      [0, 1, 0],
+      [1, 1, 1],
       [0, 0, 0]
     ]
   };
+
+  movePieceLeft() {
+    this.activePiece.x--;
+
+    if (this.hasCollision()) {
+      this.activePiece.x++;
+    }
+  }
+
+  movePieceRight() {
+    this.activePiece.x++;
+
+    if (this.hasCollision()) {
+      this.activePiece.x--;
+    }
+  }
+
+  movePieceDown() {
+    this.activePiece.y++;
+
+    if (this.hasCollision()) {
+      this.activePiece.y--;
+    }
+  }
+
+  hasCollision() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+    const playfield = this.playfield;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (
+          blocks[y][x] &&
+          ((playfield[pieceY + y] === undefined || playfield[pieceY + y][pieceX + x] === undefined) ||
+          playfield[pieceY + y][pieceX + x])
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  lockPiece() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
 }
